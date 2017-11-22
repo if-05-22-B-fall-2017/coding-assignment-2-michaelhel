1e647b90bc8c9a6d19637f9aa301a97ccf03b507
michaelhel <32167762+michaelhel@users.noreply.github.com>
2017-11-22 17:48:23 +0100

    Add files via upload

---
+++ Genalogy.pl
@@ -0,0 +1,22 @@
+parent(adam, john).
+parent(eve, john).
+parent(eve, lisa).
+parent(john, anne).
+parent(john, pat).
+parent(pat, jacob).
+parent(carol, jacob).
+
+/*
+    2: parent(X, pat).
+    3: parent(lisa, X).
+*/
+
+male(adam).
+male(john).
+male(pat).
+male(jacob).
+
+female(eve).
+female(lisa).
+female(anne).
+female(carol).
